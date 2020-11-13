a83b1829e3e351791ce85c012c81b7e25d875586
Divya <divvyatripaathi@gmail.com>
2020-11-13 20:43:19 +0100

    calculator buttons and UI

---
+++ js/init.js
@@ -0,0 +1,62 @@
+document.getElementById("clearInput").onclick = function () {
+    document.getElementById("demo").innerHTML = "0"
+
+}
+
+document.getElementById("showInput7").addEventListener("click", myFunction7);
+function myFunction7() {
+    document.getElementById("demo").innerHTML += 7
+}
+
+
+document.getElementById("showInput8").addEventListener("click", myFunction8);
+function myFunction8() {
+    document.getElementById("demo").innerHTML += 8
+}
+
+document.getElementById("showInput9").addEventListener("click", myFunction9);
+function myFunction9() {
+    document.getElementById("demo").innerHTML += 9
+}
+
+// document.getElementById("showMul").addEventListener("click, myFunctionMul");
+// function myFunctionMul() {
+// document.getElementById("demo").innerHTML = "*"
+// }
+
+document.getElementById("showInput4").addEventListener("click", myFunction4);
+function myFunction4() {
+    document.getElementById("demo").innerHTML += 4
+}
+
+document.getElementById("showInput5").addEventListener("click", myFunction5);
+function myFunction5() {
+    document.getElementById("demo").innerHTML += 5
+}
+
+document.getElementById("showInput6").addEventListener("click", myFunction6);
+function myFunction6() {
+    document.getElementById("demo").innerHTML += 6
+}
+
+
+
+document.getElementById("showInput").addEventListener("click", myFunction);
+function myFunction() {
+    document.getElementById("demo").innerHTML += 1
+}
+
+document.getElementById("showInput2").addEventListener("click", myFunction2);
+function myFunction2() {
+    document.getElementById("demo").innerHTML += 2
+}
+
+document.getElementById("showInput3").addEventListener("click", myFunction3);
+function myFunction3() {
+    document.getElementById("demo").innerHTML += 3
+}
+
+document.getElementById("showInput0").addEventListener("click", myFunction0);
+function myFunction0() {
+    document.getElementById("demo").innerHTML += 0
+}
